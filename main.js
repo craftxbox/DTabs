85d9eab3d243d3ef6c2ac2ec81c12faba457f454
craftxbox <craftxbox@craftxbox.com>
2020-05-07 03:13:45 -0230

    autoupdate p1/2

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow, session,shell} = require('electron')
+const { app, BrowserWindow, session,shell,dialog} = require('electron')
+const fs = require("fs");
 
 let mainWindow
 
@@ -16,6 +17,27 @@ function createWindow() {
     mainWindow = null
   })
   
+  if(!fs.existsSync("./resources/app/index.html")){
+    var files = fs.readdirSync("./resources/app");
+    var times = [];
+    var timeextractor = /index.html.OLD-(\d+)/;
+    for(var name of files){
+      if(!timeextractor.test(name)) continue;
+      var time = name.match(timeextractor)[1];
+      times.push((time-1)+1);
+    }
+    if(times.length < 1) {
+      debugger;
+      dialog.showMessageBox({type:"error",title:"DTabs Fatal Error",
+        message:"FATAL: index.html is missing and DTabs could not find an update to revert!\nYour installation is CORRUPT and needs reinstalling!"})
+      app.quit();
+      return;
+    }
+    times.sort((a,b)=>{return b-a;});
+    fs.renameSync("./resources/app/index.html.OLD-"+times[0],"./resources/app/index.html")
+    dialog.showMessageBox({type:"warning",title:"DTabs Warning",
+        message:"WARNING: index.html was missing but DTabs found and reverted an update!\nThis may indicate a failed update or install corruption.\nIf you experience any issues reinstall DTabs!"})
+  }
   mainWindow.loadFile("index.html")
 }
 
