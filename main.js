90123e8634bf65f651de17fbf6faeedbeb5b1598
craftxbox <craftxbox@craftxbox.com>
2023-11-05 22:25:21 -0330

    electron 27 compat

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const { app, BrowserWindow, session,shell,dialog} = require('electron')
+const { app, BrowserWindow, session, shell, dialog } = require('electron')
 const fs = require("fs");
 const path = require("path");
+require('@electron/remote/main').initialize()
+
+const version = 6;
 
 let mainWindow
 
@@ -9,39 +12,45 @@ function createWindow() {
     frame: false,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation:false,
+      contextIsolation: false,
       enableRemoteModule: true,
       webviewTag: true,
       session: session.defaultSession,
-      nativeWindowOpen:true
+      nativeWindowOpen: true
     }
   })
   mainWindow.on('closed', function () {
     mainWindow = null
   })
 
-  if(!fs.existsSync(path.resolve(__dirname, "index.html"))) {
+  if (!fs.existsSync(path.resolve(__dirname, "index.html"))) {
     var files = fs.readdirSync(__dirname);
     var times = [];
     var timeextractor = /index.html.OLD-(\d+)/;
-    for(var name of files){
-      if(!timeextractor.test(name)) continue;
+    for (var name of files) {
+      if (!timeextractor.test(name)) continue;
       var time = name.match(timeextractor)[1];
-      times.push((time-1)+1);
+      times.push((time - 1) + 1);
     }
-    if(times.length < 1) {
+    if (times.length < 1) {
       debugger;
-      dialog.showMessageBox({type:"error",title:"DTabs Fatal Error",
-        message:"FATAL: index.html is missing and DTabs could not find an update to revert!\nYour installation is CORRUPT and needs reinstalling!"})
+      dialog.showMessageBox({
+        type: "error", title: "DTabs Fatal Error",
+        message: "FATAL: index.html is missing and DTabs could not find an update to revert!\nYour installation is CORRUPT and needs reinstalling!"
+      })
       app.quit();
       return;
     }
-    times.sort((a,b)=>{return b-a;});
-    fs.renameSync(path.resolve(__dirname, "index.html.OLD-"+times[0]), path.resolve(__dirname,"index.html"))
-    dialog.showMessageBox({type:"warning",title:"DTabs Warning",
-        message:"WARNING: index.html was missing but DTabs found and reverted an update!\nThis may indicate a failed update or install corruption.\nIf you experience any issues reinstall DTabs!"})
+    times.sort((a, b) => { return b - a; });
+    fs.renameSync(path.resolve(__dirname, "index.html.OLD-" + times[0]), path.resolve(__dirname, "index.html"))
+    dialog.showMessageBox({
+      type: "warning", title: "DTabs Warning",
+      message: "WARNING: index.html was missing but DTabs found and reverted an update!\nThis may indicate a failed update or install corruption.\nIf you experience any issues reinstall DTabs!"
+    })
   }
+  mainWindow.webContents.setUserAgent(`DTabs-mainjs/${version} Electron/${process.versions.electron} Chrome/${process.versions.chrome} Node/${process.versions.node} v8/${process.versions.v8}`);
   mainWindow.loadFile("index.html")
+  require("@electron/remote/main").enable(mainWindow.webContents);
 }
 
 app.on('ready', createWindow)
@@ -52,9 +61,9 @@ app.on('window-all-closed', function () {
 })
 app.on('web-contents-created', function (webContentsCreatedEvent, contents) {
   if (contents.getType() === 'webview') {
-    contents.on('new-window', function (newWindowEvent, url) {
+    contents.setWindowOpenHandler(({ url }) => {
       shell.openExternal(url);
-      newWindowEvent.preventDefault();
+      return { action: 'deny' };
     });
   }
 });
